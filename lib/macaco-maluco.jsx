d38bf6119a04ad8137a45ef63061daf6773d6b85
Paulo Ragonha <paulo@ragonha.me>
2015-01-24 20:44:39 -0200

    we have a grid that is interactive through touch

--- lib/macaco-maluco.jsx
+++ lib/macaco-maluco.jsx
@@ -5,7 +5,121 @@ require("./macaco-maluco.scss");
 
 
 module.exports = React.createClass({
+
   render: function() {
-    return <div className="macaco-maluco"></div>;
+    window.addEventListener('touchstart', function (e) {
+      e.preventDefault();
+    });
+
+    window.addEventListener('touchmove', function (e) {
+      var touch = e.targetTouches[0] || {};
+      var el = document.elementFromPoint(touch.clientX, touch.clientY);
+      el.className = 'selected';
+    });
+
+    return <div className="macaco-maluco">
+      <table>
+        <tr>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+          <td>1</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>2</td>
+          <td>2</td>
+          <td>2</td>
+          <td>2</td>
+          <td>2</td>
+          <td>2</td>
+          <td>2</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>3</td>
+          <td>3</td>
+          <td>3</td>
+          <td>3</td>
+          <td>3</td>
+          <td>3</td>
+          <td>3</td>
+          <td>3</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>4</td>
+          <td>4</td>
+          <td>4</td>
+          <td>4</td>
+          <td>4</td>
+          <td>4</td>
+          <td>4</td>
+          <td>4</td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>5</td>
+          <td>5</td>
+          <td>5</td>
+          <td>5</td>
+          <td>5</td>
+          <td>5</td>
+          <td>5</td>
+          <td>5</td>
+        </tr>
+        <tr>
+          <td>6</td>
+          <td>6</td>
+          <td>6</td>
+          <td>6</td>
+          <td>6</td>
+          <td>6</td>
+          <td>6</td>
+          <td>6</td>
+          <td>6</td>
+        </tr>
+        <tr>
+          <td>7</td>
+          <td>7</td>
+          <td>7</td>
+          <td>7</td>
+          <td>7</td>
+          <td>7</td>
+          <td>7</td>
+          <td>7</td>
+          <td>7</td>
+        </tr>
+        <tr>
+          <td>8</td>
+          <td>8</td>
+          <td>8</td>
+          <td>8</td>
+          <td>8</td>
+          <td>8</td>
+          <td>8</td>
+          <td>8</td>
+          <td>8</td>
+        </tr>
+        <tr>
+          <td>9</td>
+          <td>9</td>
+          <td>9</td>
+          <td>9</td>
+          <td>9</td>
+          <td>9</td>
+          <td>9</td>
+          <td>9</td>
+          <td>9</td>
+        </tr>
+      </table>
+
+    </div>;
   }
 });
