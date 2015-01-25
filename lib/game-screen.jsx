fc55e42afca31b56bfb7948d9a6eb1592a580697
Paulo Ragonha <paulo@ragonha.me>
2015-01-25 19:07:18 -0200

    redirects back to the splash screen on death

--- lib/game-screen.jsx
+++ lib/game-screen.jsx
@@ -25,7 +25,7 @@ module.exports = React.createClass({
     };
 
     gameLogic.onGameOver = function () {
-      // that.transitionTo('main');
+      that.transitionTo('main');
     };
 
     that._gameLogic = gameLogic;
